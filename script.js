8bfc7db1b0c3e9730039f0fa93bb419ecebd82cc
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 11:15:01 -0300

    Arquivos Checkpoint01

---
+++ script.js
@@ -0,0 +1,39 @@
+const nome = "Iago Diniz Fontes"
+const idade = 19
+const status = true 
+const valorProduto = 10.50
+const times = ["Santos", "Celtics", "Asas São Paulo"]
+
+console.log(nome)
+console.log(idade)
+console.log(status)
+console.log(valorProduto)
+console.log(times)
+
+// Operação soma
+function soma(numero1, numero2){
+    const resultado = numero1 + numero2
+    console.log(resultado)
+}
+soma(10 ,10)
+
+// Subtração
+function subtracao(numero1, numero2){
+    const resultado = numero1 - numero2
+    console.log(resultado)
+}
+subtracao(20, 10)
+
+//Multiplicação
+function multiplicacao(numero1, numero2){
+    const resultado = numero1 * numero2
+    console.log(resultado)
+}
+multiplicacao (5, 5)
+
+//Divisão
+function divisao(numero1, numero2){
+    const resultado = numero1 / numero2
+    console.log(resultado)
+}
+divisao(10, 2)
